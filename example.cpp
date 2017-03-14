db6e0f59b475d5e9ba6d7e7725e0b661943ab0fc
e <rekarger@gmail.com>
2017-03-13 20:46:01 -0400

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -2,18 +2,11 @@
 #include "source/shellcode.hpp"
 using namespace std;
 
-/*
-http://www.i42.co.uk/stuff/hexdump.htm
-http://courses.cs.vt.edu/cs2604/spring02/Projects/2/HexDump.cpp
-*/
-
 int main(int argc, char** argv) { 
    if ( argc != 2 ) {
-      std::cout << "Invocation: hexdump <binaryfilename>" << std::endl;
+      std::cout << "Error: hexdump <binaryfilename>" << std::endl;
       return 1;
    }
    cout << payload_gen::payload(argv[1]) << endl;
-
-
    return 0;
 }
